b40c4a1ff93dadb68af3361d6a93e1e690e88670
zhang <94685912+zhangchosui@users.noreply.github.com>
2026-02-05 12:38:08 +0900

    新增基础 reset 样式文件并在页面引入

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <title>中年人控糖第一枪</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
